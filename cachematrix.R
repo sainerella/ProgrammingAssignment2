4204e7e2e81089399fd6f64ae01e74a541872d65
Sai Nerella <snerella@genscape.com>
2016-07-10 01:06:29 -0400

    Initial Commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+          invm <- NULL
+          set <- function(y){
+                    x<<-y
+                    invm<<-NULL
+          }
+          get <-function()x
+          setinverse <-function(inverse) m <<-inverse
+          getinverse <- function() invm
+          list(set=set,get=get,,setinverse=setinverse,getinverse=getinverse)
 
 }
 
